4537053e7d2b300875940f2d5f4b8875fe5a113c
minhzom <minh1382005@gmail.com>
2026-07-04 23:20:02 +0700

    first commit

---
+++ src/main/java/com/NMCNPM/ABT_bio/AbtBioApplication.java
@@ -0,0 +1,13 @@
+package com.NMCNPM.ABT_bio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbtBioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbtBioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NMCNPM/ABT_bio/AbtBioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NMCNPM.ABT_bio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbtBioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
